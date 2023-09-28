8afd1a6bbc4dc3adfd98a49eb895013dbd84ee9b
Michael Gulotta <gulottam@gmail.com>
2023-09-28 12:51:35 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,5 @@
 const Account = require('./accounts-model')
-
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
   const error = {status: 400}
@@ -26,10 +26,17 @@ exports.checkAccountPayload = (req, res, next) => {
   }
 
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log(' checkAccountNameUnique middleware')
-  next()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try { 
+    const existing = await db('accounts').where('name', req.body.name.trim()).first()
+    if(existing) {
+      next({status: 400, message:'that name is taken'})
+    } else {
+      next()
+    }
+  } catch(error) {
+    next(error)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
